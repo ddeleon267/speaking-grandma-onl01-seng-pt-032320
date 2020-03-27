21a343a7b67e1ecefdb0c20969326445198ee3e7
Dominique De León <dominique.a.deleon@gmail.com>
2020-03-27 14:03:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+     'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase.upcase != phrase 
+     "HUH?! SPEAK UP, SONNY!"
+  else
+     "NO, NOT SINCE 1938!"
+  end
+  
+end
+
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
